7de4bc6c656bd2563f549817f811650443c11f4c
julio saldanha <julio.saldanha@gorilainvest.com.br>
2021-08-08 16:53:29 -0300

    refactor: move HttpResponse for a helper

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,20 +1,17 @@
 import { MissingParamError } from '../errors/missing-param-error'
 import { HttpRequest, HttpResponse } from '../protocols/http'
+import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name')
-      }
+    const { name, email } = httpRequest.body
+
+    if (name) {
+      return badRequest(new MissingParamError('name'))
     }
 
-    if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('email')
-      }
+    if (email) {
+      return badRequest(new MissingParamError('email'))
     }
   }
 }
