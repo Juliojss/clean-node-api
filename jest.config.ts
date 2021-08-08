b0673a7667793643e734e0d1e774b43b4db13c26
julio saldanha <julio.saldanha@gorilainvest.com.br>
2021-08-08 02:18:26 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  root: ['<rootDir>/src'],
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
