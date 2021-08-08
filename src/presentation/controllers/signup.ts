1f1b97bceaaaec9ca48ae96d0868d9572ab38ee7
julio saldanha <julio.saldanha@gorilainvest.com.br>
2021-08-08 16:57:14 -0300

    refactor: simply code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,14 +4,12 @@ import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const { name, email } = httpRequest.body
+    const requiredFields = ['name', 'email']
 
-    if (name) {
-      return badRequest(new MissingParamError('name'))
-    }
-
-    if (email) {
-      return badRequest(new MissingParamError('email'))
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
