b679e69837c1f74cdb5290e2f7b2e650e42ef9f3
julio saldanha <julio.saldanha@gorilainvest.com.br>
2021-10-26 02:27:59 -0300

    refactor: create make sut email validator adapter

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -7,22 +7,32 @@ jest.mock('validator', () => ({
   }
 }))
 
+interface SutTypes {
+  sut: EmailValidatorAdapter
+}
+
+const makeSut = (): SutTypes => {
+  return {
+    sut: new EmailValidatorAdapter()
+  }
+}
+
 describe('EmailValidator Adapter', () => {
   it('Should return false if validator returns false', () => {
-    const sut = new EmailValidatorAdapter()
+    const { sut } = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBeFalsy()
   })
 
   it('Should return true if validator returns true', () => {
-    const sut = new EmailValidatorAdapter()
+    const { sut } = makeSut()
     const isValid = sut.isValid('valid_email@mail.com.br')
     expect(isValid).toBeTruthy()
   })
 
   it('Should call isEmail function with correct param', () => {
-    const sut = new EmailValidatorAdapter()
+    const { sut } = makeSut()
     const isEmailSpyOn = jest.spyOn(validator, 'isEmail')
     sut.isValid('valid_email@mail.com.br')
     expect(isEmailSpyOn).toHaveBeenCalledWith('valid_email@mail.com.br')
