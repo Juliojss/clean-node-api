3062a6f2b1eac3554aaac665b5284ae371f31551
julio saldanha <julio.saldanha@gorilainvest.com.br>
2021-10-26 02:56:37 -0300

    feat: add DbAddAccount

---
+++ src/data/usecases/add-account/db-add-account.ts
@@ -0,0 +1,16 @@
+import { AccountModel } from '../../../domain/models/account'
+import { AddAccount, AddAccountModel } from '../../../domain/usecases/add-account'
+import { Encrypter } from '../../protocols/encrypter'
+
+export class DbAddAccount implements AddAccount {
+  constructor (private readonly encrypter: Encrypter) {}
+
+  async add (account: AddAccountModel): Promise<AccountModel> {
+    const hashedPassword = await this.encrypter.encrypt(account.password)
+    return {
+      ...account,
+      id: 'any_id',
+      password: hashedPassword
+    }
+  }
+}
