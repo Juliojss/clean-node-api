5c02a17b95553e8887aacc4e480cc6b3ff52fb05
julio saldanha <juliosaldanha71@gmail.com>
2021-10-28 00:49:52 -0300

    feat: ensure AccountMongoRepository returns account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,21 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+interface AccountMongoModel {
+  _id: string
+  name: string
+  email: string
+  password: string
+}
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const { insertedId } = await accountCollection.insertOne(accountData)
+    const account = await accountCollection.findOne<AccountMongoModel>({ _id: insertedId })
+    const { _id, ...accountWithoutId } = account
+    return Object.assign({}, accountWithoutId, { id: _id })
+  }
+}
---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,17 @@
+import { MongoClient, Collection } from 'mongodb'
+
+export const MongoHelper = {
+  connection: null as MongoClient,
+
+  async connect (uri: string): Promise<void> {
+    this.connection = await MongoClient.connect(uri)
+  },
+
+  async disconnect (): Promise<void> {
+    await this.connection.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.connection.db().collection(name)
+  }
+}
