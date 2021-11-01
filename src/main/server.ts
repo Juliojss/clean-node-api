c6aab768e9ab771a6b21e04652783f92dfe33ce9
julio saldanha <juliosaldanha71@gmail.com>
2021-11-01 01:22:27 -0300

    feat: connect mongodb before run node server

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,9 @@
-import app from './config/app'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import env from './config/env'
 
-app.listen(3000, () => console.log('server running at http://localhost:3000'))
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`server running at http://localhost:${env.port}`))
+  })
+  .catch((err) => console.error(err))
